a97826cf1ccda282ebe42fa3b21a7f5bde2233c5
JoaqBS <titobarcelo@solver.cl>
2024-05-13 14:54:38 -0400

    with np

--- ddpo/sampling.py
+++ ddpo/sampling.py
@@ -4,6 +4,7 @@ import math
 
 import torch
 from tqdm.auto import tqdm
+import numpy as np
 
 from ddpo.config import EPS
 from ddpo.utils import flush
@@ -335,5 +336,6 @@ def sample_from_segments(num_samples, num_timesteps, current_iteration, target_i
     # Convert the sampled timesteps to a list of integers
     # sampled_timesteps_list = [int(x.item()) for x in sampled_timesteps]  # Convert each tensor element to int
     sampled_timesteps_list = [sampled_timesteps[i].item() for i in range(sampled_timesteps.size(0))]
+    flat_list = np.concatenate(sampled_timesteps_list).tolist()
     
-    return sampled_timesteps_list
+    return flat_list
